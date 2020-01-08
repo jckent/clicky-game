09286241c1f0ee21c4f2dc722caebd7bfa38281b
jckent <chasekent30@gmail.com>
2020-01-08 17:04:30 -0500

    working except reset message

---
+++ clicky-game/src/components/Header/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css";
+
+function Header() {
+  return (
+    <header className="header">
+      <h1>Some Skyrim NPC's</h1>
+      <h2>You can't click anyone more than once. Get the high score and become Dovahkiin!</h2>
+    </header>
+  );
+}
+
+export default Header;
